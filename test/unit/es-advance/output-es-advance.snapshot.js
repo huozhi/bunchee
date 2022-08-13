74787b6563d50a3c1b006e524d81602fb1415272
Jiachi Liu <inbox@huozhi.im>
2022-08-13 21:35:29 +0200

    rebase

--- test/unit/es-advance/output-es-advance.snapshot.js
+++ test/unit/es-advance/output-es-advance.snapshot.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var _foo$bar;
 
 var foo = {
@@ -7,13 +6,4 @@ var foo = {
 console.log('bar', foo == null ? void 0 : foo.bar);
 var prop = (_foo$bar = foo.bar) != null ? _foo$bar : 'default';
 console.log('name', prop);
-=======
-var foo = {
-    bar: "hello"
-};
-console.log("bar", foo === null || foo === void 0 ? void 0 : foo.bar);
-var _bar;
-var prop = (_bar = foo.bar) !== null && _bar !== void 0 ? _bar : "default";
-console.log("name", prop);
->>>>>>> 6bf6570 (Use manual snapshot)
 //# sourceMappingURL=bundle.js.map
--- test/unit/shebang/output-shebang.snapshot.js
+++ test/unit/shebang/output-shebang.snapshot.js
@@ -1,7 +1,3 @@
 #!/usr/bin/env node
-<<<<<<< HEAD
 console.log('shebang');
-=======
-console.log("shebang");
->>>>>>> 6bf6570 (Use manual snapshot)
 //# sourceMappingURL=bundle.js.map
