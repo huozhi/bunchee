729e1feab52e80da774a424ea7cde5b17ea4db92
Jiachi Liu <inbox@huozhi.im>
2022-10-03 15:52:26 +0200

    Add test for bunchee node api

---
+++ test/typing.test.ts
@@ -0,0 +1,19 @@
+// Import from generated files instead of using tsconfig alias
+import { bundle, type CliArgs } from '../dist/lib'
+
+describe('types', () => {
+  it('should be able to import the node API and use correct typings', async () => {
+    const options: CliArgs = {
+      dts: false,
+      watch: false,
+      minify: false,
+      sourcemap: false,
+      external: [],
+      format: 'esm',
+      target: 'es2016',
+      runtime: 'nodejs',
+    }
+
+    return () => bundle('./tmp/index.ts', options)
+  })
+})
