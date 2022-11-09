1655d7bfe56ce2ec938c25519d4797b9c635e744
Jiachi Liu <inbox@huozhi.im>
2022-11-09 02:38:36 +0100

    Fix type paths with cwd option (#143)
    * Fix type paths with cwd option
    
    * fix typing

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -10,6 +10,7 @@ const fixturesDir = resolveFromTest('fixtures')
 const testCases: {
   name: string
   args: string[]
+  dist?: string
   expected(f: string, { stderr, stdout }: { stderr: string; stdout: string }): [boolean, boolean][]
 }[] = [
   {
@@ -125,15 +126,29 @@ const testCases: {
       ]
     },
   },
+  {
+    name: 'workspace',
+    // --cwd without -o option
+    dist: path.join(fixturesDir, './package/dist'),
+    args: ['index.ts', '--cwd', path.join(fixturesDir, './package')],
+    expected(distFile) {
+      return [
+        [fs.existsSync(distFile), true],
+        // same name with input file
+        [fs.existsSync(distFile.replace('.mjs', '.d.ts')), true],
+      ]
+    }
+  },
 ]
 
 for (const testCase of testCases) {
-  const { name, args, expected } = testCase
+  const { name, args, expected, dist } = testCase
   test(`cli ${name} should work properly`, async () => {
-    // Delete dist folder (as last argument)
-    const dist = args[args.length - 1]
+    // Delete the of dist folder: folder of dist file (as last argument) or `dist` option
+    const distDir = dist || path.dirname(args[args.length - 1])
     // TODO: specify working directory for each test
-    execSync(`rm -rf ${path.dirname(dist)}`)
+    execSync(`rm -rf ${distDir}`)
+    console.log(`Command: rm -rf ${distDir}`)
     console.log(`Command: bunchee ${args.join(' ')}`)
     const ps = fork(`${__dirname + '/../node_modules/.bin/tsx'}`, [__dirname + '/../cli.ts'].concat(args), { stdio: 'pipe' })
     let stderr = ''
