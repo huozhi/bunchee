ad1ea41bc1b5247ba4fc1b138cb5185ab02e9a97
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-08 04:17:45 +0900

    Revert "chore: add warning on the last console"
    This reverts commit d8cf0dd44de1ecbd10d64a1d1041e778491cb7a4.

--- src/cli.ts
+++ src/cli.ts
@@ -147,9 +147,8 @@ async function run(args: CliArgs) {
 
   let timeStart = Date.now()
   let timeEnd
-  let result
   try {
-    result = await bundle(entry, bundleConfig)
+    await bundle(entry, bundleConfig)
     timeEnd = Date.now()
   } catch (err: any) {
     if (err.name === 'NOT_EXISTED') {
@@ -170,14 +169,6 @@ async function run(args: CliArgs) {
   logger.log(`✨  Finished in ${formatDuration(duration)}`)
 
   await lintPackage(cwd)
-
-  if (result.length === 0) {
-    console.warn(
-      'Warning: The "src" directory does not contain any entry files. ' +
-        'For proper usage, please refer to the following link: ' +
-        'https://github.com/huozhi/bunchee?tab=readme-ov-file#usage',
-    )
-  }
 }
 
 async function main() {
