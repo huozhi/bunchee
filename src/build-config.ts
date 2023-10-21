5ccc9bf6756a1ee7a306b99e5c097fbd537487fc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-21 10:46:33 -0700

    comment

--- src/build-config.ts
+++ src/build-config.ts
@@ -97,9 +97,7 @@ function buildInputConfig(
   const swcParserConfig = {
     syntax: useTypescript ? 'typescript' : 'ecmascript',
     [useTypescript ? 'tsx' : 'jsx']: true,
-    // privateMethod: true,
-    // classPrivateProperty: true,
-    // exportDefaultFrom: true,
+    exportDefaultFrom: true,
   } as const
 
   const swcOptions: import('@swc/types').Options = {
