09b7636ad157deadd0bc7ccf17936c1106bec3db
Jiachi Liu <inbox@huozhi.im>
2023-12-20 19:32:06 +0100

    Disable moduleSideEffects

--- src/build-config.ts
+++ src/build-config.ts
@@ -210,6 +210,7 @@ async function buildInputConfig(
     plugins,
     treeshake: {
       propertyReadSideEffects: false,
+      moduleSideEffects: false,
     },
     onwarn(warning, warn) {
       const code = warning.code || ''
