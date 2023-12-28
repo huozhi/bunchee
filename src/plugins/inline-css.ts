50d57b7689322bf8b7b62b5e53057e3d7cc5fc9b
Jiachi Liu <inbox@huozhi.im>
2023-12-28 01:09:10 +0100

    Fix css helper

--- src/plugins/inline-css.ts
+++ src/plugins/inline-css.ts
@@ -20,7 +20,7 @@ const helpers = {
 function __insertCSS(code) {
   if (!code || typeof document == 'undefined') return
   let head = document.head || document.getElementsByTagName('head')[0]
-  style = document.createElement('style')
+  let style = document.createElement('style')
   style.type = 'text/css'
   head.appendChild(style)
   ;style.styleSheet ? (style.styleSheet.cssText = code) : style.appendChild(document.createTextNode(code))
