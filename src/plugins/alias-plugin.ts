7655b09019e94dfd524dc09fd60fe24c2748ffa3
Jiachi Liu <inbox@huozhi.im>
2023-12-29 17:52:55 +0100

    Filter special export type out from alias (#355)

--- src/plugins/alias-plugin.ts
+++ src/plugins/alias-plugin.ts
@@ -4,7 +4,7 @@ import { Plugin } from 'rollup'
 // e.g.
 // For a resolved file, if it's one of the entries,
 // aliases it as export path, such as <absolute file> -> <pkg>/<export path>
-export function aliasEntries({ entries }: { entries: Record<string, string>}): Plugin {
+export function aliasEntries({ entries }: { entries: Record<string, string | null> }): Plugin {
   return {
     name: 'alias',
     resolveId: {
