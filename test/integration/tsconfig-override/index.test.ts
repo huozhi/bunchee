6a0105b4eef785070b3167a93adac521ca3cca6b
juliusmarminge <julius0216@outlook.com>
2024-03-14 00:10:24 +0100

    nice

--- test/integration/tsconfig-override/index.test.ts
+++ test/integration/tsconfig-override/index.test.ts
@@ -3,7 +3,30 @@ import { join } from 'path'
 import { createIntegrationTest } from '../utils'
 
 describe('integration tsconfig-override', () => {
-  const dir = __dirname
+  beforeAll(async () => {
+    await fsp.writeFile(
+      join(__dirname, './package.json'),
+      JSON.stringify({
+        name: 'tsconfig-override',
+        type: 'module',
+        exports: {
+          '.': {
+            default: './dist/index.js',
+            types: './dist/index.d.ts',
+          },
+        },
+      }),
+    )
+    await fsp.writeFile(
+      join(__dirname, './tsconfig.json'),
+      JSON.stringify({ compilerOptions: { target: 'esnext' } }),
+    )
+    await fsp.writeFile(
+      join(__dirname, './tsconfig.build.json'),
+      JSON.stringify({ compilerOptions: { target: 'es5' } }),
+    )
+  })
+
   it('should use esnext output in build without override', async () => {
     await createIntegrationTest(
       {
@@ -14,7 +37,7 @@ describe('integration tsconfig-override', () => {
           join(dir, './dist/index.js'),
           'utf-8',
         )
-        expect(content).toContain('export { index as default };')
+        expect(content).toContain("var index = (()=>'index');")
       },
     )
   })
@@ -29,7 +52,7 @@ describe('integration tsconfig-override', () => {
           join(dir, './dist/index.js'),
           'utf-8',
         )
-        expect(content).toContain("var index = (()=>'index');")
+        expect(content).toContain('export { index as default };')
       },
     )
   })
