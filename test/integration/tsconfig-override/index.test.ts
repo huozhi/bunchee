7f9baeb4c39ae939b6ce68bf61ce858c0ecfdefc
juliusmarminge <julius0216@outlook.com>
2024-03-14 00:11:24 +0100

    oki

--- test/integration/tsconfig-override/index.test.ts
+++ test/integration/tsconfig-override/index.test.ts
@@ -3,30 +3,6 @@ import { join } from 'path'
 import { createIntegrationTest } from '../utils'
 
 describe('integration tsconfig-override', () => {
-  beforeAll(async () => {
-    await fsp.writeFile(
-      join(__dirname, './package.json'),
-      JSON.stringify({
-        name: 'tsconfig-override',
-        type: 'module',
-        exports: {
-          '.': {
-            default: './dist/index.js',
-            types: './dist/index.d.ts',
-          },
-        },
-      }),
-    )
-    await fsp.writeFile(
-      join(__dirname, './tsconfig.json'),
-      JSON.stringify({ compilerOptions: { target: 'esnext' } }),
-    )
-    await fsp.writeFile(
-      join(__dirname, './tsconfig.build.json'),
-      JSON.stringify({ compilerOptions: { target: 'es5' } }),
-    )
-  })
-
   it('should use esnext output in build without override', async () => {
     await createIntegrationTest(
       {
