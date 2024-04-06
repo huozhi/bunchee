a9110b8945fedbb7f69b05b91f25c4ddae9e57e6
devjiwonchoi <devjiwonchoi@gmail.com>
2024-04-06 21:09:53 +0900

    test: add --dts test

--- test/cli/dts/index.test.ts
+++ test/cli/dts/index.test.ts
@@ -3,17 +3,35 @@ import path from 'path'
 import { createCliTest } from '../utils'
 
 describe('cli', () => {
-  it(`cli dts should work properly`, async () => {
+  it('should generate .d.ts file on JavaScript file with --dts flag', async () => {
     await createCliTest(
       {
         directory: __dirname,
-        args: ['./input.ts', '-o', 'dist/output.js'],
+        args: ['./input.js', '-o', 'dist/javascript.js'],
       },
       ({ code, distFile }) => {
         const typeFile = distFile.replace('.js', '.d.ts')
 
-        expect(path.basename(distFile)).toBe('output.js')
-        expect(path.basename(typeFile)).toBe('output.d.ts')
+        expect(path.basename(distFile)).toBe('javascript.js')
+        expect(path.basename(typeFile)).toBe('javascript.d.ts')
+        expect(fs.existsSync(distFile)).toBe(true)
+        expect(fs.existsSync(typeFile)).toBe(true)
+        expect(code).toBe(0)
+      },
+    )
+  })
+
+  it('should generate .d.ts file on TypeScript file', async () => {
+    await createCliTest(
+      {
+        directory: __dirname,
+        args: ['./input.ts', '-o', 'dist/typescript.js'],
+      },
+      ({ code, distFile }) => {
+        const typeFile = distFile.replace('.js', '.d.ts')
+
+        expect(path.basename(distFile)).toBe('typescript.js')
+        expect(path.basename(typeFile)).toBe('typescript.d.ts')
         expect(fs.existsSync(distFile)).toBe(true)
         expect(fs.existsSync(typeFile)).toBe(true)
         expect(code).toBe(0)
