46743115e9d980ea6efcace952d54e132cddb1f7
Jiachi Liu <inbox@huozhi.im>
2024-08-11 16:18:54 +0200

    test: add missing test (#571)

--- test/integration/multi-entries/index.test.ts
+++ test/integration/multi-entries/index.test.ts
@@ -1,4 +1,9 @@
-import { createIntegrationTest } from '../utils'
+import {
+  assertFilesContent,
+  createIntegrationTest,
+  getChunkFileNamesFromLog,
+  stripANSIColor,
+} from '../utils'
 
 describe('integration multi-entries', () => {
   it('should contain files', async () => {
@@ -6,7 +11,42 @@ describe('integration multi-entries', () => {
       {
         directory: __dirname,
       },
-      async ({}) => {},
+      async ({ dir, stdout }) => {
+        const contentsRegex = {
+          './dist/index.js': /'index'/,
+          './dist/shared/index.mjs': /'shared'/,
+          './dist/shared/edge-light.mjs': /'shared.edge-light'/,
+          './dist/server/edge.mjs': /'server.edge-light'/,
+          './dist/server/react-server.mjs': /'server.react-server'/,
+          // types
+          './dist/server/index.d.ts': `export { Client } from '../client/index.js';\nexport { Shared } from '../shared/index.js';`,
+          './dist/client/index.d.ts': `export { Shared } from '../shared/index.js'`,
+        }
+
+        await assertFilesContent(dir, contentsRegex)
+
+        const log = `\
+        dist/index.js
+        dist/index.d.ts
+        dist/lite.js
+        dist/client/index.mjs
+        dist/client/index.cjs
+        dist/client/index.d.ts
+        dist/server/index.mjs
+        dist/server/index.d.ts
+        dist/server/edge.mjs
+        dist/server/react-server.mjs
+        dist/shared/index.mjs
+        dist/shared/index.cjs
+        dist/shared/index.d.ts
+        dist/shared/edge-light.mjs
+        `
+
+        const rawStdout = stripANSIColor(stdout)
+        getChunkFileNamesFromLog(log).forEach((chunk: string) => {
+          expect(rawStdout).toContain(chunk)
+        })
+      },
     )
   })
 })
