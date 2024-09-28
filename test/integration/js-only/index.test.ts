4d33a99b24f713a87a14834b7eb1afb0671e4552
Jiachi Liu <inbox@huozhi.im>
2024-09-28 23:40:28 +0200

    add test

--- test/integration/js-only/index.test.ts
+++ test/integration/js-only/index.test.ts
@@ -1,6 +1,18 @@
 import { createIntegrationTest, existsFile } from '../utils'
 
 describe('integration js-only', () => {
+  beforeEach(() => {
+    // Mock the 'typescript' module to throw an error on import
+    jest.mock('typescript', () => {
+      throw new Error('typescript module should not be imported')
+    })
+  })
+
+  afterEach(() => {
+    // Restore the original module after each test
+    jest.resetModules() // Reset the module registry to ensure clean state
+    jest.clearAllMocks() // Clear mocks after each test
+  })
   it('should work with js only project', async () => {
     await createIntegrationTest(
       {
