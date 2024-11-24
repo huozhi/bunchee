3ca1818722b159e446b2fbd03c0fbb8faf3c4300
Jiachi Liu <inbox@huozhi.im>
2024-11-24 15:57:49 +0100

    fix: prepend plugin sourcemap (#603)

--- src/plugins/prepend-directives.ts
+++ src/plugins/prepend-directives.ts
@@ -1,8 +1,9 @@
 import type { Plugin } from 'rollup'
+import MagicString from 'magic-string'
 
 export function prependDirectives(): Plugin {
   return {
-    name: 'prependDirective',
+    name: 'prepend-directive',
     transform: {
       order: 'post',
       handler(code, id) {
@@ -13,7 +14,12 @@ export function prependDirectives(): Plugin {
           if (firstDirective) {
             const directive = firstDirective.value
             const directiveCode = `'${directive}';`
-            return directiveCode + '\n' + code
+            const magicString = new MagicString(code)
+            magicString.prepend(directiveCode + '\n')
+            return {
+              code: magicString.toString(),
+              map: magicString.generateMap({ hires: true }),
+            }
           }
         }
         return null
