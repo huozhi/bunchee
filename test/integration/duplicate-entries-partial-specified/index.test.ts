2f6b1b4e568d6f704754f1db327e5dc9c0c1ea5a
Jiachi Liu <inbox@huozhi.im>
2024-11-24 18:53:01 +0100

    update test

---
+++ test/integration/duplicate-entries-partial-specified/index.test.ts
@@ -0,0 +1,17 @@
+import { createIntegrationTest, getFileNamesFromDirectory } from '../utils'
+
+describe('integration duplicate-entries-partial-specified', () => {
+  it('should not generate js types paths if not specified', async () => {
+    await createIntegrationTest(
+      {
+        directory: __dirname,
+      },
+      async ({ distDir }) => {
+        expect(await getFileNamesFromDirectory(distDir)).toEqual([
+          'a.js',
+          'a.workerd.js',
+        ])
+      },
+    )
+  })
+})
