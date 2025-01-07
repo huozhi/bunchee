3699488ac31cdeb3a9917c715a32af3d5b872191
Jiachi Liu <inbox@huozhi.im>
2025-01-07 10:48:20 +0100

    test: add test for server directives

---
+++ test/integration/mixed-directives/mixed-directives.test.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from 'fs'
+import {
+  createIntegrationTest,
+  existsFile,
+  getFileNamesFromDirectory,
+} from '../utils'
+
+describe('integration - mixed-directives', () => {
+  it('should work with js only project', async () => {
+    await createIntegrationTest(
+      {
+        directory: __dirname,
+      },
+      async ({ distDir }) => {
+        const distFiles = await getFileNamesFromDirectory(distDir)
+        const fileContents = distFiles
+          .map((file) => [file, readFileSync(`${distDir}/${file}`, 'utf-8')])
+          .reduce((acc, pair) => {
+            return {
+              ...acc,
+              [pair[0]]: pair[1],
+            }
+          }, {})
+
+        expect(fileContents).toMatchSnapshot(``)
+      },
+    )
+  })
+})
