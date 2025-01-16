487ab06f2d590d8205778cef692243d83655cf98
Jiachi Liu <inbox@huozhi.im>
2025-01-17 00:09:18 +0100

    docs: improve website styling

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,10 +1,25 @@
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
 import React from 'react'
 import './globals.css'
 
 export default function Layout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
     </html>
   )
 }
--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -11,16 +11,17 @@ export default function Page() {
       </nav>
       <p>{`Build JS package with ease, package.json as configuration`}</p>
       <h3>Installation</h3>
-      <pre>
+      <pre className="code-block">
         <code>{`npm install --save-dev bunchee typescript`}</code>
       </pre>
 
       <h3>Configuration</h3>
       <p>
-        Create entry files of your library and <code>package.json</code>.
+        Create entry files of your library and{' '}
+        <code className="code--inline">package.json</code>.
       </p>
 
-      <pre>
+      <pre className="code-block">
         <code>
           {`$ cd ./coffee
 $ mkdir src && touch ./src/index.ts`}
@@ -28,10 +29,10 @@ $ mkdir src && touch ./src/index.ts`}
       </pre>
 
       <p>
-        Add the exports in <code>package.json</code>.
+        Add the exports in <code className="code--inline">package.json</code>.
       </p>
 
-      <pre>
+      <pre className="code-block">
         <code>
           {`{
   "name": "coffee",
@@ -45,12 +46,12 @@ $ mkdir src && touch ./src/index.ts`}
       </pre>
 
       <h4>Build</h4>
-      <pre>
+      <pre className="code-block">
         <code>{`$ npm run build`}</code>
       </pre>
 
       <h4>Output</h4>
-      <pre>
+      <pre className="code-block">
         <code>
           {`$ Exports  File             Size
 $ .        dist/index.js    5.6 kB`}
