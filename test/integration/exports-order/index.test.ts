2ef6bd83a1542489706c830ce4b4bcd5f4ac7092
Alex Yang <himself65@outlook.com>
2025-02-10 19:55:25 +0800

    fix: prefer to use the same exports name if possible

---
+++ test/integration/exports-order/index.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from 'vitest'
+import {
+  createJob,
+  assertContainFiles,
+  assertFilesContent,
+} from '../../testing-utils'
+
+describe('integration exports-order', () => {
+  const { distDir } = createJob({ directory: __dirname })
+
+  it('should work with exports order', async () => {
+    const distFiles = [
+      'a.cjs',
+      'a.d.cts',
+      'a.d.ts',
+      'a.edge-light.d.ts',
+      'a.edge-light.js',
+      'a.js',
+      'index.cjs',
+      'index.d.cts',
+      'index.d.ts',
+      'index.edge-light.d.ts',
+      'index.edge-light.js',
+      'index.js',
+    ]
+    await assertContainFiles(distDir, distFiles)
+    await assertFilesContent(distDir, {
+      'a.cjs': `const foo = 'foo';
+
+exports.foo = foo;`,
+      'a.edge-light.js': `const foo = 'foo';
+
+export { foo };`,
+      'a.js': `const foo = 'foo';
+
+export { foo };`,
+      'index.cjs': `var a_cjs = require('./a.cjs');`,
+      'index.edge-light.js': `export * from './a.edge-light.js';`,
+      'index.js': `export * from './a.js';`,
+    })
+  })
+})
