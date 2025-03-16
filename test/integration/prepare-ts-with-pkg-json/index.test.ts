cf5a5b2e3b32725a46e2e7e9bf9ade508f9e6e84
Jiachi Liu <inbox@huozhi.im>
2025-03-16 14:08:08 +0100

    fix: prepare overriding conditions

--- test/integration/prepare-ts-with-pkg-json/index.test.ts
+++ test/integration/prepare-ts-with-pkg-json/index.test.ts
@@ -5,27 +5,68 @@ import { assertContainFiles, createJob, deleteFile } from '../../testing-utils'
 
 describe('integration prepare-ts-with-pkg-json', () => {
   const dir = __dirname
-  beforeAll(async () => {
-    await fsp.writeFile(
-      join(dir, './package.json'),
-      '{ "name": "prepare-ts-with-pkg-json" }',
-    )
-    await deleteFile(join(dir, './tsconfig.json'))
+
+  describe('with no exports', () => {
+    beforeAll(async () => {
+      await fsp.writeFile(
+        join(dir, './package.json'),
+        JSON.stringify({ name: 'prepare-ts-with-pkg-json' }, null, 2),
+      )
+      await deleteFile(join(dir, './tsconfig.json'))
+    })
+    createJob({
+      directory: __dirname,
+      args: ['prepare'],
+    })
+    it('should contain output files', async () => {
+      assertContainFiles(dir, ['package.json'])
+      const pkgJson = JSON.parse(
+        await fsp.readFile(join(dir, './package.json'), 'utf-8'),
+      )
+      expect(pkgJson.files).toContain('dist')
+      expect(pkgJson.type).toBeUndefined()
+      expect(pkgJson.main).toBe('./dist/cjs/index.js')
+      expect(pkgJson.module).toBe('./dist/es/index.mjs')
+    })
   })
 
-  createJob({
-    directory: __dirname,
-    args: ['prepare'],
-  })
-
-  it('should contain files', async () => {
-    assertContainFiles(dir, ['package.json'])
-    const pkgJson = JSON.parse(
-      await fsp.readFile(join(dir, './package.json'), 'utf-8'),
-    )
-    expect(pkgJson.files).toContain('dist')
-    expect(pkgJson.type).toBeUndefined()
-    expect(pkgJson.main).toBe('./dist/cjs/index.js')
-    expect(pkgJson.module).toBe('./dist/es/index.mjs')
+  describe('with existing exports', () => {
+    beforeAll(async () => {
+      await fsp.writeFile(
+        join(dir, './package.json'),
+        JSON.stringify(
+          {
+            name: 'prepare-ts-with-pkg-json',
+            exports: {
+              '.': {
+                default: './dist/index.js',
+              },
+            },
+          },
+          null,
+          2,
+        ),
+      )
+      await deleteFile(join(dir, './tsconfig.json'))
+    })
+    createJob({
+      directory: __dirname,
+      args: ['prepare'],
+    })
+    it('should not override existing exports field', async () => {
+      assertContainFiles(dir, ['package.json'])
+      const pkgJson = JSON.parse(
+        await fsp.readFile(join(dir, './package.json'), 'utf-8'),
+      )
+      expect(pkgJson.files).toContain('dist')
+      expect(pkgJson.type).toBeUndefined()
+      expect(pkgJson.main).toBe('./dist/cjs/index.js')
+      expect(pkgJson.module).toBe('./dist/es/index.mjs')
+      expect(pkgJson.exports).toEqual({
+        '.': {
+          default: './dist/index.js',
+        },
+      })
+    })
   })
 })
