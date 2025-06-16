4e087ea70fe06b66511cc406c80f67ad3ad81116
Jiachi Liu <inbox@huozhi.im>
2025-06-16 10:06:54 +0200

    fix: camel case the identifier name

--- src/utils.ts
+++ src/utils.ts
@@ -48,6 +48,12 @@ export function fileExists(filePath: string) {
   return fs.existsSync(filePath)
 }
 
+const toCamelCase = (str: string): string => {
+  return str
+    .replace(/[-_]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''))
+    .replace(/^[A-Z]/, (c) => c.toLowerCase())
+}
+
 // . -> pkg name
 // ./lite -> <pkg name>/lite
 export function getExportPath(
@@ -55,7 +61,7 @@ export function getExportPath(
   cwd: string,
   exportName?: string,
 ) {
-  const name = pkg.name || path.basename(cwd)
+  const name = toCamelCase(pkg.name || path.basename(cwd))
   if (exportName === '.' || !exportName) return name
   return path.join(name, exportName)
 }
