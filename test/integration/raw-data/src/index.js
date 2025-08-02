70963b23bcc220f55f1cf37b025d9822087f41fc
Jiachi Liu <inbox@huozhi.im>
2025-08-02 18:20:13 +0200

    feat: support raw query to load file as raw content (#694)

--- test/integration/raw-data/src/index.js
+++ test/integration/raw-data/src/index.js
@@ -1,3 +1,5 @@
 import content from './content.txt'
+import markdownContent from './readme.md?raw'
 
 export const data = content
+export const rawMarkdown = markdownContent
---
+++ test/integration/raw-data/src/readme.md
@@ -0,0 +1,3 @@
+# Test File
+
+This is a test markdown file for the `?raw` query parameter feature. 
